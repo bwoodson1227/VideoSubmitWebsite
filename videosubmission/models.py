620003bf13b2d95fb32049f52eff780248180b68
B Woodson <38901286+bwoodson1227@users.noreply.github.com>
2023-07-07 19:14:37 -0400

    Update models.py

--- videosubmission/models.py
+++ videosubmission/models.py
@@ -8,3 +8,14 @@ class Video(models.models:
 
     def __str__(self):
         return self.title
+
+
+from django.db import models
+
+class Video(models.models:
+    title = models.Charfield(max_length=200)
+    video_file = models.FileField(upload_to'videos/')
+    submitted_at = modelsDateTimeField(auto_now_add=TRUE)
+
+    def __str__(self):
+        return self.title
